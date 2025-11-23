4d8fe96b58fc4b14d47c69448c4d78ddd4280559
Alejandro Amaya <a.amayaa2@uniandes.edu.co>
2025-11-23 02:30:06 -0500

    Cambios, se me olvidó subir un repo

---
+++ src/uniandes/dpoo/swing/interfaz/agregar/PanelEditarRestaurante.java
@@ -0,0 +1,94 @@
+package uniandes.dpoo.swing.interfaz.agregar;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+@SuppressWarnings("serial")
+public class PanelEditarRestaurante extends JPanel
+{
+    /**
+     * El campo para que el usuario ingrese el nombre del restaurante
+     */
+    private JTextField txtNombre;
+
+    /**
+     * Un selector (JComboBox) para que el usuario seleccione la calificación (1 a 5) del restaurante
+     */
+    private JComboBox<String> cbbCalificacion;
+
+    /**
+     * Un selector (JComboBox) para que el usuario indique si ya visitó el restaurante o no
+     */
+    private JComboBox<String> cbbVisitado;
+
+    public PanelEditarRestaurante( )
+    {
+    	setLayout( new GridLayout( 3, 2 ) );
+        // Crea el campo para el nombre con una etiqueta al frente
+        // TODO completar
+    	JLabel labNombre = new JLabel( "Nombre:" );
+        txtNombre = new JTextField( 20 );
+        this.add( labNombre );
+        this.add( txtNombre );
+
+        // Crea el selector para la calificación con una etiqueta al frente
+        // TODO completar
+        JLabel labCalificacion = new JLabel( "Calificación:" );
+        cbbCalificacion = new JComboBox<String>( );
+        cbbCalificacion.addItem( "1" );
+        cbbCalificacion.addItem( "2" );
+        cbbCalificacion.addItem( "3" );
+        cbbCalificacion.addItem( "4" );
+        cbbCalificacion.addItem( "5" );
+        this.add( labCalificacion );
+        this.add( cbbCalificacion );
+
+        // Crea el selector para indicar si ya ha sido visitado, con una etiqueta al frente
+        // TODO completar
+        JLabel labVisitado = new JLabel( "Visitado:" );
+        cbbVisitado = new JComboBox<String>( );
+        cbbVisitado.addItem( "Sí" );
+        cbbVisitado.addItem( "No" );
+
+        // Agregar todos los elementos al panel
+        // TODO completar
+        this.add( labVisitado );
+        this.add( cbbVisitado );
+
+    }
+
+    /**
+     * Indica si en el selector se seleccionó la opción que dice que el restaurante fue visitado
+     * @return
+     */
+    public boolean getVisitado( )
+    {
+        String sel = ( String )cbbVisitado.getSelectedItem( );
+        return sel.equalsIgnoreCase( "Sí" );
+    }
+
+    /**
+     * Indica la calificación marcada en el selector
+     * @return
+     */
+    public int getCalificacion( )
+    {
+        String calif = ( String )cbbCalificacion.getSelectedItem( );
+        return Integer.parseInt( calif );
+    }
+
+    /**
+     * Indica el nombre digitado para el restaurante
+     * @return
+     */
+    public String getNombre( )
+    {
+        return txtNombre.getText( );
+    }
+
+}
